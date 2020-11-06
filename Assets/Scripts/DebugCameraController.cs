6da23172f5645d411c452a99e90f85f47f45c447
Joseph DeVictoria <jldevictoria@gmail.com>
2020-11-06 15:17:09 -0800

    Adding a basic scene, some opengameart CC0 or public domain assets.

---
+++ Assets/Scripts/DebugCameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugCameraController : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0.0f);
+        UnityEngine.Debug.Log(transform.position);
+        transform.position = transform.position + movement * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/ScrollSpace.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollSpace : MonoBehaviour
+{
+    public float parralax = 3.0f;
+    private MeshRenderer meshRenderer;
+    private Material material;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        material = meshRenderer.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = material.mainTextureOffset;
+        offset.x = transform.position.x / transform.localScale.x / parralax;
+        offset.y = transform.position.y / transform.localScale.y / parralax;
+        material.mainTextureOffset = offset;
+    }
+}
