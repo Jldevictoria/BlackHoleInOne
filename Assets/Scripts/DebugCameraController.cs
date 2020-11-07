b1b5ca78e1aeb3ddc4255b4797a42793801c5f24
Joseph DeVictoria <jldevictoria@gmail.com>
2020-11-06 20:05:58 -0800

    Adding some different controls to allow zooming and panning camera. Decoupled from moonball.

--- Assets/Scripts/DebugCameraController.cs
+++ Assets/Scripts/DebugCameraController.cs
@@ -5,11 +5,34 @@ using UnityEngine;
 public class DebugCameraController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float cameraZoomSpeed = 1.0f;
+    public float cameraPanSpeed = 0.1f;
 
+    private Vector3 previousMousePosition;
+    private bool captureMouseMovement;
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0.0f);
-        transform.position = transform.position + movement * Time.deltaTime;
+        //Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0.0f);
+        // Handle zooming camera with scroll wheel.
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) {
+            Camera.main.orthographicSize -= cameraZoomSpeed;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0) {
+            Camera.main.orthographicSize += cameraZoomSpeed;
+        }
+        // Handle panning camera with mouse click and drag.
+        if (Input.GetMouseButtonDown(0)) {
+            previousMousePosition = Input.mousePosition;
+            captureMouseMovement = true;
+        }
+        if (Input.mousePosition != previousMousePosition && captureMouseMovement) {
+            Vector3 deltaMousePosition = Input.mousePosition - previousMousePosition;
+            previousMousePosition = Input.mousePosition;
+            transform.position -= deltaMousePosition * cameraPanSpeed;
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            captureMouseMovement = false;
+        }
     }
 }
---
+++ Assets/Scripts/OrbitObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitObject : MonoBehaviour
+{
+    public GameObject targetBody;
+    public float radius = 10.0f;
+    public bool clockwise = false;
+    public float skew = 0.0f;
+    public float orbitSpeed = 1.0f;
+    public float startAngle = 0.0f;
+    
+    private float angle = 0.0f;
+    private float direction = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        angle = startAngle;
+        if (clockwise) {
+            direction = -1.0f;
+        }
+        else {
+            direction = 1.0f;
+        }
+        Update();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle += direction * Time.deltaTime * orbitSpeed;
+        Vector3 targetPosition = targetBody.transform.position;
+        targetPosition.x += Mathf.Sin(angle) * radius;
+        targetPosition.y += Mathf.Cos(angle) * radius;
+        transform.position = targetPosition;
+    }
+}
