b3dc04d420ad50e43acf5985798d67d00ce16c7b
Joseph DeVictoria <jldevictoria@gmail.com>
2020-11-06 22:11:22 -0800

    Adding pool/golf shot mechanic.  Added line to represent your swing.

--- Assets/Scripts/DebugCameraController.cs
+++ Assets/Scripts/DebugCameraController.cs
@@ -7,32 +7,51 @@ public class DebugCameraController : MonoBehaviour
     public float speed = 5.0f;
     public float cameraZoomSpeed = 1.0f;
     public float cameraPanSpeed = 0.1f;
+    public float cameraZoomMax = 100;
+    public float cameraZoomMin = 5;
+    public GameObject moonBall;
 
     private Vector3 previousMousePosition;
     private bool captureMouseMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(moonBall.transform.position.x + 5, moonBall.transform.position.y, transform.position.z);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        //Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0.0f);
         // Handle zooming camera with scroll wheel.
-        if (Input.GetAxis("Mouse ScrollWheel") > 0) {
+        if ((Input.GetAxis("Mouse ScrollWheel") > 0) && (Camera.main.orthographicSize >= cameraZoomMin)) {
             Camera.main.orthographicSize -= cameraZoomSpeed;
         }
-        else if (Input.GetAxis("Mouse ScrollWheel") < 0) {
+        else if ((Input.GetAxis("Mouse ScrollWheel") < 0) && (Camera.main.orthographicSize <= cameraZoomMax)) {
             Camera.main.orthographicSize += cameraZoomSpeed;
         }
+
         // Handle panning camera with mouse click and drag.
         if (Input.GetMouseButtonDown(0)) {
-            previousMousePosition = Input.mousePosition;
-            captureMouseMovement = true;
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider == null) {
+                previousMousePosition = Input.mousePosition;
+                captureMouseMovement = true;
+            }
         }
         if (Input.mousePosition != previousMousePosition && captureMouseMovement) {
             Vector3 deltaMousePosition = Input.mousePosition - previousMousePosition;
             previousMousePosition = Input.mousePosition;
             transform.position -= deltaMousePosition * cameraPanSpeed;
         }
-        if (Input.GetMouseButtonUp(0)) {
+        if (Input.GetMouseButtonUp(0) && captureMouseMovement) {
             captureMouseMovement = false;
+        }
+
+        if (moonBall.GetComponent<Rigidbody2D>().velocity != Vector2.zero) {
+            transform.position = new Vector3(moonBall.transform.position.x, moonBall.transform.position.y, transform.position.z);
         }
     }
 }
---
+++ Assets/Scripts/MoonBallController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonBallController : MonoBehaviour
+{
+    public float power;
+
+    private Rigidbody2D rigidBody2D;
+    private LineRenderer lineRenderer;
+    private GameObject line;
+    private Vector3 startMousePosition;
+    private bool captureMouseMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Handle panning camera with mouse click and drag.
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null) {
+                if (hit.collider.gameObject == gameObject) {
+                    UnityEngine.Debug.Log("Player clicked on " + gameObject.name);
+                    startMousePosition = transform.position;
+                    captureMouseMovement = true;
+                }
+            }
+        }
+        if (Input.GetMouseButton(0) && captureMouseMovement) {
+            Vector3 currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            lineRenderer.SetPosition(0, new Vector3(startMousePosition.x, startMousePosition.y, 0.0f));
+            lineRenderer.SetPosition(1, new Vector3(currentMousePos.x, currentMousePos.y, 0.0f));
+        }
+        if (Input.GetMouseButtonUp(0) && captureMouseMovement) {
+            Vector3 delta = startMousePosition - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            rigidBody2D.velocity = new Vector2((delta.x * power), (delta.y * power));
+            lineRenderer.SetPosition(0, Vector3.zero);
+            lineRenderer.SetPosition(1, Vector3.zero);
+            captureMouseMovement = false;
+        }
+    }
+}
