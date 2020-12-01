52bc42309266ebe71532bfc6f1ba421ccd666d31
Joseph DeVictoria <jldevictoria@gmail.com>
2020-12-01 12:24:44 -0800

    Removing level 7, because it wasnt right.

---
+++ Assets/RestartLevelController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartLevelController : MonoBehaviour
+{
+    public GameObject gc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gc = GameObject.Find("GameController");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
